fdd94b947ad194ddd4aced5c4bdf1cfa1847847d
Garik363 <Crimea363@gmail.com>
2023-06-14 18:20:33 +0300

    Сделаны 2  задачи

--- dz5.py
+++ dz5.py
@@ -6,14 +6,21 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8 
 
-# def stepen(a, b):
-#     if (b == 1):
-#         return (a)
-#     if (b != 1):
-#         return (a * stepen(a, b - 1))
-# a = int(input("Введите число: "))
-# b = int(input("Введите его степень: "))
-# print("Результат возведения в степень равен:", stepen(a, b))
+def stepen(a, b):
+    if b>0:
+        if (b == 1):
+            return (a)
+        if (b != 1):
+            return (a * stepen(a, b - 1))
+    
+    else:
+        if b == -1:
+            return (1/a)
+        if (b != -1):
+            return (1/(a * stepen(a, b + 1)))
+a = int(input("Введите число: "))
+b = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", stepen(a, b))
 
 # Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
@@ -22,17 +29,17 @@
 # 2 2
 #     4 
 
-def sum(a, b):
-    if b > 0:
-        if b == 1:
-            return a + 1
+# def sum(a, b):
+#     if b > 0:
+#         if b == 1:
+#             return a + 1
     
-        return sum(a, b -1) +1
-    else:
-      if b == -1:
-            return a - 1
-      return sum(a, b +1) -1
+#         return sum(a, b -1) +1
+#     else:
+#       if b == -1:
+#             return a - 1
+#       return sum(a, b +1) -1
     
-a = int(input())
-b = int(input())
-print(sum(a, b))
+# a = int(input())
+# b = int(input())
+# print(sum(a, b))
